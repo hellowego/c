8cc846b84a3ebd9e8b2b288a4b7e9c8197e79022
CF <hellowego@gmail.com>
2024-11-03 12:19:03 +0800

    45

---
+++ jump_game_45.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int dfs(int *nums, int numsSize, int index)
+{
+    if (index == numsSize)
+    {
+        return 0;
+    }
+    return 0;
+}
+
+bool canJump(int *nums, int numsSize)
+{
+    return 0;
+}
+
+int jump(int *nums, int numsSize)
+{
+    int min = 0;
+
+    return 0;
+}
+
+int main()
+{
+    int nums[] = {2, 3, 1, 1, 4};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d  ", nums[i]);
+    }
+    printf("\n");
+    int ans = jump(nums, 5);
+
+    return 0;
+}
